635da6691614e20c2bc9016b00cf16aa48092787
Steve Roche <1steveroche@gmail.com>
2018-06-10 22:36:47 +0530

    Edits

--- registration.php
+++ registration.php
@@ -4,34 +4,30 @@
 	<title> Registration Portal </title>
 </head>
 <body>
-	
 	<div>
-		<form action="" method="post" name="registration">
+		<form action="upload.php" method="POST" name="registration">
 			<div>
-			<input type="text" name="t_name" placeholder="Team Name"> 
-			<input type="text" name="t_head" placeholder="Team Head"> 
-			<input type="text" name="reg_no" placeholder="Registration Number"> 
+				<input type="text" name="t_name" placeholder="Team Name"/>
+				<input type="text" name="t_head" placeholder="Team Head"/>
+				<input type="text" name="reg_no" placeholder="Registration Number"/>
 			</div>
 
 			<div>
-				<input type="text" placeholder="Branch/Specialisation" name="branch"> 
-				<input type="text" placeholder="Semester" name="sem">
-				<input type="text" placeholder="Institution" name="insti">
+				<input type="text" placeholder="Branch/Specialisation" name="branch"/>
+				<input type="text" placeholder="Semester" name="sem"/>
+				<input type="text" placeholder="Institution" name="insti"/>
 			</div>
 
 			<div>
-				<input type="text" placeholder="Phone Number" name="ph_no">
-				<input type="tex" placeholder="Email Id" name="em_id">
+				<input type="text" placeholder="Phone Number" name="ph_no"/>
+				<input type="text" placeholder="Email Id" name="em_id"/>
 			</div>
 
 			<div>
-				<input type="file" name="abstract_file">
+				<input type="file" name="abstract_file"/>
 			</div>
 
-			<button> submit </button>
-
-
-
+			<input type="submit" name="submit" value="Register"/>
 		</form>
 	</div>
 </body>

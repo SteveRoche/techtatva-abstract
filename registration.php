4cb1363a51a3feb970825e420d6760a3460cf732
aditi-g21 <goyal.aditi121@gmail.com>
2018-06-13 21:00:06 +0530

    minor changes

--- registration.php
+++ registration.php
@@ -1,37 +1,49 @@
+<?php
+	session_start();
+?>
 <!DOCTYPE html>
 <html>
 <head>
 	<title> Registration Portal </title>
+
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300">
+
 </head>
+
 <body>
-	
+
+	<header class="heading">
+		<h1>REGISTRATION PAGE</h1>
+	</header>
+
 	<div>
-		<form action="" method="post" name="registration">
-			<div>
-			<input type="text" name="t_name" placeholder="Team Name"> 
-			<input type="text" name="t_head" placeholder="Team Head"> 
-			<input type="text" name="reg_no" placeholder="Registration Number"> 
-			</div>
-
-			<div>
-				<input type="text" placeholder="Branch/Specialisation" name="branch"> 
-				<input type="text" placeholder="Semester" name="sem">
-				<input type="text" placeholder="Institution" name="insti">
-			</div>
-
-			<div>
-				<input type="text" placeholder="Phone Number" name="ph_no">
-				<input type="tex" placeholder="Email Id" name="em_id">
-			</div>
-
-			<div>
-				<input type="file" name="abstract_file">
-			</div>
-
-			<button> submit </button>
+		<img src="tt.svg" id="logo">
+	</div>
 
 
+	<div class="choices">
+		<form action="db.php" method="POST" name="registration">
+				<input type="text" name="t_name" placeholder="Team Name" pattern="[A-Za-z0-9 ]+" required/>
+				<input type="text" name="t_head" placeholder="Team Head" pattern="[A-Za-z.' ]+" required/>
+				<input type="text" name="reg_no" placeholder="Registration Number" pattern="[0-9]{9}" required/>
+				<input type="text" placeholder="Branch/Specialisation" name="branch" pattern="[A-Za-z\x26. ]+" required/>
+				<input type="text" placeholder="Semester" name="sem" pattern="[1-9]" required/>
+				<input type="text" placeholder="Institution" name="insti" pattern="[A-Za-z\x26. ]+" required/>
+				<input type="text" placeholder="Phone Number" name="ph_no" pattern="[0-9]{10}" required/>
+				<input type="email" placeholder="Email Id" name="em_id" required/>
+				<input type="file" name="abstract_file" required/>
 
+				<input type="submit" name="submit" value="Register"/>
+			<?php
+				if(isset($_SESSION['message']))
+				{
+					echo "<br>";
+					$message=$_SESSION['message'];
+					echo "$message";
+				}
+			?>
 		</form>
 	</div>
 </body>

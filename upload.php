b4d2103cab2e8955dcd20b3c108882de7b5f8e6a
Arsh-Agrawal <arsh.167.agrawal@gmail.com>
2018-06-13 20:46:41 +0530

    minor changes again

--- upload.php
+++ upload.php
@@ -9,7 +9,8 @@
 
 	$dsn = "mysql:host=$dbHost;dbname=$dbName;charset=$charset";
 
-	$opt = [
+	$opt =
+	[
 		PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
 		PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
 		PDO::ATTR_EMULATE_PREPARES => false,
@@ -20,7 +21,8 @@
 		"INSERT INTO abstract_teams(team_name, team_head, reg_no, branch, sem, institution, phone_no, email, file_path) VALUES (?,?,?,?,?,?,?,?,?)"
 	);
 
-	if(isset($_POST['submit'])) {
+	if(isset($_POST['submit']))
+	{
 
 		$formOk = false;
 		$uploadOk = false;
@@ -39,23 +41,40 @@
 		$phone_no = $_POST['ph_no'];
 		$email = $_POST['em_id'];
 
-		if (empty($team_name)) {
+		if (empty($team_name)) 
+		{
 			$message = "Team name required";
-		} elseif (empty($team_head)) {
+		} 
+		elseif (empty($team_head)) 
+		{
 			$message = "Team head required";
-		} elseif (empty($reg_no)) {
+		} 
+		elseif (empty($reg_no)) 
+		{
 			$message = "registeration number required";
-		} elseif (empty($branch)) {
+		} 
+		elseif (empty($branch)) 
+		{
 			$message = "Branch/Specialisation required";
-		} elseif (empty($sem)) {
+		} 
+		elseif (empty($sem)) 
+		{
 			$message = "Semester required";
-		} elseif (empty($institution)) {
+		} 
+		elseif (empty($institution))
+		{
 			$message = "Institution required";
-		} elseif (empty($phone_no)) {
+		} 
+		elseif (empty($phone_no)) 
+		{
 			$message = "Phone number required";
-		} elseif (empty($email)) {
+		} 
+		elseif (empty($email)) 
+		{
 			$message = "Email required";
-		} else {
+		} 
+		else 
+		{
 			$formOk = true;
 		}
 
@@ -66,27 +85,41 @@
 
 			$errCode = $_FILES['abstract_file']['error'];
 			$uploadFilePath = $uploadDir . basename($_FILES['abstract_file']['name']);
-			if ($errCode != UPLOAD_ERR_OK && $errCode != UPLOAD_ERR_FORM_SIZE && $errCode != UPLOAD_ERR_INI_SIZE) {
+			if ($errCode != UPLOAD_ERR_OK && $errCode != UPLOAD_ERR_FORM_SIZE && $errCode != UPLOAD_ERR_INI_SIZE) 
+			{
 				$uploadMessage = "Failed to upload file.";
-			} elseif ($errCode == UPLOAD_ERR_INI_SIZE || $errCode == UPLOAD_ERR_FORM_SIZE || $_FILES['abstract_file']['size'] > (5 * 1024 * 1024)) {
+			} 
+			elseif ($errCode == UPLOAD_ERR_INI_SIZE || $errCode == UPLOAD_ERR_FORM_SIZE || $_FILES['abstract_file']['size'] > (5 * 1024 * 1024)) 
+			{
 				$uploadMessage = "File too large";
-			} elseif (strtolower(pathinfo($uploadFilePath, PATHINFO_EXTENSION)) != 'pdf') {
+			}
+			elseif (strtolower(pathinfo($uploadFilePath, PATHINFO_EXTENSION)) != 'pdf') 
+			{
 				$uploadMessage = "Not a pdf";
-			} elseif (file_exists($uploadFilePath)) {
+			}
+			elseif (file_exists($uploadFilePath)) 
+			{
 				$uploadMessage = "File already exists";
-			} elseif (!move_uploaded_file($_FILES['abstract_file']['tmp_name'], $uploadFilePath)) {
+			}
+			elseif (!move_uploaded_file($_FILES['abstract_file']['tmp_name'], $uploadFilePath)) 
+			{
 				$uploadMessage = "Failed to upload file";
-			} else {
+			}
+			else 
+			{
 				$uploadOk = true;
 				$uploadMessage = 'Upload is Ok';
 			}
 		}
 
-		if ($formOk && $uploadOk) {
+		if ($formOk && $uploadOk) 
+		{
 			$stmtAddTeam->execute([$team_name, $team_head, $reg_no, $branch, $sem, $institution, $phone_no, $email, $uploadFilePath]);
 			echo "Registered successfully";
 			session_destroy();
-		} else {
+		}
+		else 
+		{
 			$_SESSION['message'] = $message;
 			$_SESSION['upload_message'] = $uploadMessage;
 			header("Location: registration.php");
